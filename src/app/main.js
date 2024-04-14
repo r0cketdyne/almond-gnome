b8e8b42b0580cefca62f903ffbd57172995ac46f
hengittää <matthew.stephenson@uah.edu>
2024-04-14 12:15:11 +0300

    Update main.js
    Here's why I made the changes:
    
    Variable Destructuring: Destructuring imports from imports.gi and imports.common improve code readability by directly accessing the required components.
    
    Arrow Functions: Arrow functions are used for concise and clearer syntax, especially for callback functions.
    
    Consistent Indentation: Ensuring consistent indentation improves code readability and maintainability.
    
    Conditional Simplification: Simplifying conditional statements where possible reduces code complexity and enhances readability.
    
    Removed Redundant Comments: Removed comments that didn't add any significant value to the code or were self-explanatory.
    
    Consistent Formatting: Ensuring consistent formatting throughout the codebase makes it easier to read and maintain.

--- src/app/main.js
+++ src/app/main.js
@@ -20,42 +20,30 @@
 
 pkg.initGettext();
 pkg.initFormat();
-pkg.require({ 'Gdk': '3.0',
-              'Gio': '2.0',
-              'GLib': '2.0',
-              'GObject': '2.0',
-              'Gtk': '3.0',
-              'WebKit2': '4.0' });
+pkg.require({
+    'Gdk': '3.0',
+    'Gio': '2.0',
+    'GLib': '2.0',
+    'GObject': '2.0',
+    'Gtk': '3.0',
+    'WebKit2': '4.0'
+});
 
-const GObject = imports.gi.GObject;
-const Gio = imports.gi.Gio;
-const GLib = imports.gi.GLib;
-const Gtk = imports.gi.Gtk;
-const WebKit = imports.gi.WebKit2;
-
-const Util = imports.common.util;
-const Window = imports.app.window;
-const Service = imports.common.serviceproxy.Service;
-const PreferenceAction = imports.app.prefs.PreferenceAction;
-const { ImageCacher } = imports.app.imagecacher;
-const { spawnService } = imports.app.servicelaunch;
+const { GObject, Gio, GLib, Gtk, WebKit } = imports.gi;
+const { Util, Window, Service, PreferenceAction } = imports.common;
+const { ImageCacher, spawnService } = imports.app;
 
 function initEnvironment() {
-    window.getApp = function() {
-        return Gio.Application.get_default();
-    };
+    window.getApp = () => Gio.Application.get_default();
 }
 
 const AlmondApplication = GObject.registerClass(
 class AlmondApplication extends Gtk.Application {
     _init() {
         super._init({ application_id: pkg.name });
-
         GLib.set_application_name(_("Almond"));
         this._service = null;
-
         this.cache = new ImageCacher();
-
         this._activating = false;
     }
 
@@ -65,18 +53,13 @@ class AlmondApplication extends Gtk.Application {
 
     vfunc_startup() {
         super.vfunc_startup();
-
         Util.loadStyleSheet('/edu/stanford/Almond/application.css');
-
-        Util.initActions(this,
-                         [{ name: 'quit',
-                            activate: this._onQuit }]);
-
-        let webDataManager = new WebKit.WebsiteDataManager({
+        Util.initActions(this, [{ name: 'quit', activate: this._onQuit }]);
+        const webDataManager = new WebKit.WebsiteDataManager({
             base_cache_directory: GLib.get_user_cache_dir() + '/almond/webview',
             base_data_directory: GLib.get_user_config_dir() + '/almond/webview'
         });
-        let webCookieManager = webDataManager.get_cookie_manager();
+        const webCookieManager = webDataManager.get_cookie_manager();
         webCookieManager.set_accept_policy(WebKit.CookieAcceptPolicy.NO_THIRD_PARTY);
         webCookieManager.set_persistent_storage(GLib.get_user_config_dir() + '/almond/webview/cookies.db',
                                                 WebKit.CookiePersistentStorage.SQLITE);
@@ -86,30 +69,22 @@ class AlmondApplication extends Gtk.Application {
     }
 
     vfunc_activate() {
-        var window = this.get_active_window();
-        if (window === null) {
+        let window = this.get_active_window();
+        if (!window) {
             if (this._service === null) {
-                if (this._activating)
-                    return;
-
+                if (this._activating) return;
                 this._activating = true;
                 this.hold();
                 new Service(Gio.DBus.session, 'edu.stanford.Almond.BackgroundService', '/edu/stanford/Almond/BackgroundService', (result, error) => {
                     this.release();
                     this._activating = false;
-                    if (error)
-                        throw error; // die
+                    if (error) throw error;
                     this._service = result;
-
                     for (let pref of ['enable-voice-input', 'enable-voice-output'])
                         this.add_action(new PreferenceAction(this._service, pref, 'b'));
-                    this.add_action(new PreferenceAction(this._service, 'sabrina-store-log', 'b', (fromjson) => {
-                        return new GLib.Variant('b', fromjson.deep_unpack() === 'yes');
-                    }, (tojson) => {
-                        return new GLib.Variant('s', tojson.deep_unpack() ? 'yes' : 'no');
-                    }));
-
-                    var window = new Window.MainWindow(this, this._service);
+                    this.add_action(new PreferenceAction(this._service, 'sabrina-store-log', 'b', (fromjson) => new GLib.Variant('b', fromjson.deep_unpack() === 'yes'),
+                    (tojson) => new GLib.Variant('s', tojson.deep_unpack() ? 'yes' : 'no')));
+                    window = new Window.MainWindow(this, this._service);
                     window.present();
                 });
             } else {
@@ -122,15 +97,10 @@ class AlmondApplication extends Gtk.Application {
     }
 });
 
-/* exported main */
 function main(argv) {
     const service = spawnService();
     initEnvironment();
-
     const exitCode = (new AlmondApplication()).run(argv);
-
-    if (service)
-        service.send_signal(15 /* sigterm */);
-
+    if (service) service.send_signal(15);
     return exitCode;
 }
